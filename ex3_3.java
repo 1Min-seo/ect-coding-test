f5e17fd39c579c2ba186903b38e0a193b48f0390
Minseo Han <readex11@naver.com>
2023-12-24 16:18:25 +0900

    숫자 카드 게임

---
+++ ex3_3.java
@@ -0,0 +1,33 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ex3_3 {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		
+		int num=0;
+		int result=0;
+		
+		for(int i=0; i<n; i++) {
+			int min=10001;  
+			st=new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				num=Integer.parseInt(st.nextToken());
+				min=Math.min(min, num);
+			}
+			result=Math.max(min, result);
+		}
+		
+		System.out.println(result);
+	}
+
+}
